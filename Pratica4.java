f514d8dac63dae896bee83932db6e523132709a4
Vinicius Lazarotti <viniciuslazarotti@gmail.com>
2026-03-28 10:28:30 -0300

    Pratica4.java

---
+++ Pratica4.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pratica4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Este é um programa de cálculos envolvendo círculos.");
+        System.out.print("Opções: \n1 - Calcula o perímetro \n2 - Calcula a área \n3 - Calcula o volume");
+        System.out.println("Qual operação você quer fazer? Digite 1, 2 ou 3: ");
+        int opcao = teclado.nextInt(); // Guarda a opção escolhida
+        System.out.println("Digite o valor do raio: ");
+        double raio = teclado.nextDouble(); // Guarda o valor do raio desse círculo
+
+        switch(opcao) {
+            case 1:
+                double perimetro = 2 * 3.141592 * raio;
+                System.out.println("Perímetro do círculo: " + perimetro);
+                break;
+            
+            case 2:
+                double area = 3.141592 * (raio * raio);
+                System.out.println("Área do círculo: " + area);
+                break;
+
+            case 3:
+                double volume = 4 / 3 * (3.141592 * (raio * raio * raio));
+                System.out.print("Volume do círculo: " + volume);
+                break;
+
+            default:
+                System.out.print("Erro. Código de operação inválido.");
+        }
+
+
+        teclado.close();
+    }
+}
