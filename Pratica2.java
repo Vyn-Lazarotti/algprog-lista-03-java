818e6e233d011e47ee4569e29e16092899849a90
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-03-30 19:09:21 -0300

    Pratica2.java

--- Pratica2.java
+++ Pratica2.java
@@ -4,7 +4,7 @@ public class Pratica2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Este é um programa que simula uma máquina automatizada de venda de lanchinhos");
-        int troco, valorCompra, pagamento, nota50, nota20, nota10, nota5, nota2, nota1;
+        int troco, valorCompra, pagamento, nota50, nota20, nota10, nota5, nota2, moeda1;
 
         System.out.println("Bem-vindo ao Gavv Snack! :D");
         System.out.println("Esta máquina aceita apenas notas de R$ 50,00, R$ 20,00, R$ 10,00, R$ 5,00, R$ 2,00 e R$ 1,00.");
@@ -22,10 +22,10 @@ public class Pratica2 {
         nota5 = teclado.nextInt();
         System.out.print("Nota de 2: ");
         nota2 = teclado.nextInt();
-        System.out.print("Nota de 1: ");
-        nota1 = teclado.nextInt();
+        System.out.print("Moeda de 1: ");
+        moeda1 = teclado.nextInt();
 
-        pagamento = (nota50 * 50) + (nota20 * 20) + (nota10 * 10) + (nota5 * 5) + (nota2 * 2) + (nota1 * 1);
+        pagamento = (nota50 * 50) + (nota20 * 20) + (nota10 * 10) + (nota5 * 5) + (nota2 * 2) + (moeda1 * 1);
 
         troco = pagamento - valorCompra;
 
